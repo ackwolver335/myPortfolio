25d7097032d5eefa8102a82a450a3b704e175574
ackwolver335 <abhaych335@gmail.com>
2025-05-23 08:51:54 +0530

    Some Page Fixation regarding Header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,27 @@
 // importing required elements
+import { useEffect, useState } from "react"
 
 export default function Navbar() {
+
+    // regarding Page's title
+    const [title,setTitle] = useState("Home");
+
+    // useEffect regarding changing Page's Title
+    useEffect(() => {
+        document.title = `My Portfolio - ${title}`;
+    },[title]);
+
     return (
         <div className = "navBar z-20 fixed w-full bg-white/80 backdrop-blur-2xl flex md:flex-row justify-between items-center xsz:py-3 xsz:px-4 md:px-5 xl:px-9 2xl:px-14 2xl:py-5 xl:py-4 xsz:shadow-md xl:shadow-lg">
 
-            <h1 className = "xsz:text-lg xl:text-xl xsz:font-bold text-site font-poppins cursor-pointer"> Portfolio </h1>
+            <h1 className = "xsz:text-lg xl:text-2xl xsz:font-bold text-site font-poppins cursor-pointer"> Portfolio </h1>
 
             <ul className = "flex md:flex-row font-poppins md:text-sm md:gap-4 2xl:gap-6 xl:text-base text-secondary">
-                <li> <a href = "#" className = "hover:text-site ease-in duration-150 hover:text-shadow-black"> Home </a> </li>
-                <li> <a href = "#about" className = "hover:text-site ease-in duration-150 hover:text-shadow-black"> About </a> </li>
-                <li> <a href = "#skills" className = "hover:text-site ease-in duration-150 hover:text-shadow-black"> Skills </a> </li>
-                <li> <a href = "#projects" className = "hover:text-site ease-in duration-150 hover:text-shadow-black"> Projects </a> </li>
-                <li> <a href = "#contact" className = "hover:text-site ease-in duration-150 hover:text-shadow-black"> Contact </a> </li>
+                <li> <a href = "#" className = "hover:text-site ease-in duration-150 hover:text-shadow-black" onClick={() => {setTitle("Home") }}> Home </a> </li>
+                <li> <a href = "#about" className = "hover:text-site ease-in duration-150 hover:text-shadow-black" onClick={() => {setTitle("About") }}> About </a> </li>
+                <li> <a href = "#skills" className = "hover:text-site ease-in duration-150 hover:text-shadow-black" onClick={() => {setTitle("Skills") }}> Skills </a> </li>
+                <li> <a href = "#projects" className = "hover:text-site ease-in duration-150 hover:text-shadow-black" onClick={() => {setTitle("Projects") }}> Projects </a> </li>
+                <li> <a href = "#contact" className = "hover:text-site ease-in duration-150 hover:text-shadow-black" onClick={() => {setTitle("Contact") }}> Contact </a> </li>
             </ul>
 
         </div>
