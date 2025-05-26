47465a3a9e59a8371a4c9e3dd163ce853ab28eb9
ackwolver335 <abhaych335@gmail.com>
2025-05-26 19:48:05 +0530

    Contact Section Mobile Layout Optimized

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,57 +2,57 @@ import { Phone, Mail, MapPin, Twitter } from 'lucide-react'
 
 export default function Contact() {
     return (
-        <div className="contentContainer md:py-16 lg:py-20 xl:py-24 flex flex-col md:gap-4 lg:gap-6 xl:gap-7 items-center bg-tempBG" id="contact">
+        <div className="contentContainer xsz:py-14 sm:py-18 xsz:gap-y-5 md:py-16 lg:py-20 xl:py-24 flex flex-col md:gap-4 lg:gap-6 xl:gap-7 items-center bg-tempBG" id="contact">
 
             {/* Heading of Contact Section */}
-            <div className="mainHead flex flex-col items-center md:gap-2 lg:gap-3">
-                <h1 className="font-inter md:text-2xl lg:text-3xl xl:text-4xl text-secondary font-bold tracking-wide"> Get In Touch </h1>
-                <div className="md:h-[3px] md:w-20 lg:w-24 xl:w-28 md:rounded-md md:drop-shadow-lg bg-site"> </div>
+            <div className="mainHead flex flex-col xsz:gap-y-1 items-center md:gap-2 lg:gap-3">
+                <h1 className="font-inter xsz:text-2xl sm:text-3xl xl:text-4xl text-secondary font-bold tracking-wide"> Get In Touch </h1>
+                <div className="xsz:h-1 xsz:w-20 sm:w-24 xsz:rounded-xl md:h-[3px] md:w-20 lg:w-24 xl:w-28 md:rounded-md xsz:drop-shadow-md md:drop-shadow-lg bg-site"> </div>
             </div>
 
             {/* Para regarding Section's Intro */}
-            <h4 className="text-secondary/60 font-poppins md:text-sm xl:text-base"> Have a project in mind or want to collaborate? Feel freet to reach out! </h4>
+            <h4 className="text-secondary/60 font-poppins md:text-sm xsz:text-[12px] sm:text-sm xl:text-base"> Have a project in mind or want to collaborate? Feel freet to reach out! </h4>
 
-            <div className="contactSection grid md:grid-cols-1 lg:grid-cols-2 md:pt-8 xl:pt-10 md:gap-y-8 lg:gap-x-14 xl:gap-x-20">
+            <div className="contactSection grid md:grid-cols-1 lg:grid-cols-2 xsz:pt-5 md:pt-8 xl:pt-10 xsz:gap-y-10 md:gap-y-8 lg:gap-x-14 xl:gap-x-20">
 
-                <div className="contactInfo flex flex-col justify-start bg-white md:rounded-md lg:rounded-xl lg:shadow-lg md:shadow-md md:px-8 md:py-6 w-100 md:gap-5 xl:gap-7">
+                <div className="contactInfo flex flex-col justify-start bg-white xsz:rounded-lg xsz:shadow-lg md:rounded-md lg:rounded-xl lg:shadow-lg md:shadow-md xsz:px-6 xsz:py-4 sm:px-8 sm:py-6 md:px-8 md:py-6 sm:w-100 xsz:gap-4 md:gap-5 xl:gap-7">
 
-                    <h3 className="md:text-xl lg:text-2xl font-inter md:font-semibold text-secondary/90"> Contact Information </h3>
+                    <h3 className="xsz:text-lg sm:text-xl lg:text-2xl font-inter xsz:font-bold xsz:text-secondary/80 sm:font-semibold sm:text-secondary/90"> Contact Information </h3>
 
-                    <div className="details flex flex-col justify-start md:gap-5 xl:gap-7">
+                    <div className="details flex flex-col justify-start xsz:gap-4 md:gap-5 xl:gap-7">
 
                         {/* Email Detail's Block */}
-                        <div className="emailID flex flex-row justify-start md:gap-5 items-center">
+                        <div className="emailID flex flex-row justify-start xsz:gap-3 sm:gap-4 md:gap-5 items-center">
 
-                            <Mail className="text-site bg-site/25 md:w-12 md:h-12 md:px-3 md:rounded-full" />
+                            <Mail className="text-site bg-site/25 xsz:w-8 xsz:h-8 sm:w-10 sm:h-10 sm:p-2 sm:rounded-xl xsz:p-1 xsz:rounded-md md:w-12 md:h-12 md:px-3 md:rounded-full" />
 
-                            <div className="md:space-y-1">
-                                <h4 className="font-inter font-semibold text-secondary/80 md:text-lg"> Email </h4>
-                                <p className="font-poppins font-normal text-secondary md:text-sm"> abhaych335@gmail.com </p>
+                            <div className="xsz:space-y-1 sm:space-y-0 md:space-y-1">
+                                <h4 className="font-inter font-semibold text-secondary/80 md:text-lg xsz:text-base"> Email </h4>
+                                <p className="font-poppins font-normal text-secondary text-sm"> abhaych335@gmail.com </p>
                             </div>
 
                         </div>
 
                         {/* Phone/Contact Details Block */}
-                        <div className="phoneNB flex flex-row justify-start md:gap-5 items-center">
+                        <div className="phoneNB flex flex-row justify-start xsz:gap-3 sm:gap-4 md:gap-5 items-center">
 
-                            <Phone className="text-site bg-site/25 md:w-12 md:h-12 md:px-3 md:rounded-full" />
+                            <Phone className="text-site bg-site/25 xsz:w-8 xsz:h-8 sm:w-10 sm:h-10 sm:p-2 sm:rounded-xl xsz:p-1 xsz:rounded-md md:w-12 md:h-12 md:px-3 md:rounded-full" />
 
-                            <div className="md:space-y-1">
-                                <h4 className="font-inter font-semibold text-secondary/80 md:text-lg"> Phone </h4>
-                                <p className="font-poppins font-normal text-secondary md:text-sm"> +91-8130621593 </p>
+                            <div className="xsz:space-y-1 sm:space-y-0 md:space-y-1">
+                                <h4 className="font-inter font-semibold text-secondary/80 md:text-lg xsz:text-base"> Phone </h4>
+                                <p className="font-poppins font-normal text-secondary text-sm"> +91-8130621593 </p>
                             </div>
 
                         </div>
 
                         {/* Location or Nearby Address Detail's Block */}
-                        <div className="adddressBD flex flex-row justify-start md:gap-5 items-center">
+                        <div className="adddressBD flex flex-row justify-start xsz:gap-3 sm:gap-4 md:gap-5 items-center">
 
-                            <MapPin className="text-site bg-site/25 md:w-12 md:h-12 md:px-3 md:rounded-full" />
+                            <MapPin className="text-site bg-site/25 xsz:w-8 xsz:h-8 sm:w-10 sm:h-10 sm:p-2 sm:rounded-xl xsz:p-1 xsz:rounded-md md:w-12 md:h-12 md:px-3 md:rounded-full" />
 
-                            <div className="md:space-y-1">
-                                <h4 className="font-inter font-semibold text-secondary/80 md:text-lg"> Location </h4>
-                                <p className="font-poppins font-normal text-secondary md:text-sm"> New Delhi, Delhi - India </p>
+                            <div className="xsz:space-y-1 sm:space-y-0 md:space-y-1">
+                                <h4 className="font-inter font-semibold text-secondary/80 md:text-lg xsz:text-base"> Location </h4>
+                                <p className="font-poppins font-normal text-secondary text-sm"> New Delhi, Delhi - India </p>
                             </div>
 
                         </div>
@@ -60,29 +60,29 @@ export default function Contact() {
                     </div>
 
                     {/* Social Links */}
-                    <div className="socialIcon md:space-y-3 lg:space-y-5 xl:space-y-4">
+                    <div className="socialIcon xsz:space-y-4 md:space-y-3 lg:space-y-5 xl:space-y-4">
 
-                        <h4 className="md:text-lg lg:text-xl font-inter text-secondary/80 font-semibold"> Connect With Me </h4>
+                        <h4 className="xsz:text-base sm:text-lg lg:text-xl font-inter text-secondary/80 xsz:font-bold sm:font-semibold"> Connect With Me </h4>
 
-                        <div className="flex flex-row md:gap-4">
+                        <div className="flex flex-row xsz:gap-3 sm:gap-5 md:gap-4">
 
                             {/* Twitter Link or Reference */}
                             <a href="https://x.com/AbhayCh84760">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-twitter-x md:w-12 md:h-12 md:px-3 bg-site/15 hover:bg-site/30 ease-in duration-150 rounded-full text-site" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-twitter-x xsz:w-10 xsz:h-10 xsz:p-2 active:scale-90 md:w-12 md:h-12 md:px-3 bg-site/15 hover:bg-site/30 ease-in duration-150 rounded-full text-site" viewBox="0 0 16 16">
                                     <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
                                 </svg>
                             </a>
 
                             {/* LinkdIn Profile Link */}
                             <a href="https://www.linkedin.com/in/abhaychaudhary335/">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-linkedin md:w-12 md:h-12 md:px-3 bg-site/15 hover:bg-site/30 ease-in duration-150 rounded-full text-site" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-linkedin xsz:w-10 xsz:h-10 xsz:p-2 active:scale-90 md:w-12 md:h-12 md:px-3 bg-site/15 hover:bg-site/30 ease-in duration-150 rounded-full text-site" viewBox="0 0 16 16">
                                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                                 </svg>
                             </a>
 
                             {/* Github Profile or Dashboard Link */}
                             <a href="https://github.com/ackwolver335">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-github md:w-11 md:h-11 md:px-2 bg-site/15 hover:bg-site/30 ease-in duration-150 rounded-full text-site" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-github xsz:w-10 xsz:h-10 xsz:p-2 active:scale-90 md:w-11 md:h-11 md:px-2 bg-site/15 hover:bg-site/30 ease-in duration-150 rounded-full text-site" viewBox="0 0 16 16">
                                     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
                                 </svg>
                             </a>
@@ -94,37 +94,37 @@ export default function Contact() {
                 </div>
 
                 {/* Block in order to retrive details from the User */}
-                <div className="detailedForm flex flex-col justify-start bg-white md:rounded-md lg:rounded-xl lg:shadow-lg md:shadow-md md:px-8 md:py-6 w-100 md:gap-5">
+                <div className="detailedForm flex flex-col justify-start bg-white xsz:rounded-lg xsz:shadow-lg md:rounded-md lg:rounded-xl lg:shadow-lg md:shadow-md xsz:px-6 xsz:py-4 sm:px-8 sm:py-6 md:px-8 xsz:gap-3 md:py-6 sm:w-100 md:gap-5">
 
-                    <h3 className="md:text-xl lg:text-2xl font-inter md:font-semibold text-secondary/90"> Send Me a Message </h3>
+                    <h3 className="xsz:text-lg sm:text-xl lg:text-2xl font-inter xsz:font-bold xsz:text-secondary/80 sm:font-semibold text-secondary/90"> Send Me a Message </h3>
 
                     {/* form regarding information retrival */}
-                    <form className="flex flex-col md:gap-y-3 lg:gap-y-5 justify-start">
-                        
+                    <form className="flex flex-col xsz:gap-3 md:gap-y-3 lg:gap-y-5 justify-start">
+
                         {/* userName */}
-                        <div className="flex flex-col justify-start md:gap-y-2">
-                            <label htmlFor="userName" className="font-inter font-normal text-secondary/80 md:text-sm lg:text-base"> Name </label>
-                            <input type="text" placeholder="Your Name" id="userName" className = "font-poppins md:text-sm border-2 md:opacity-80  border-secondary/50 focus:border-2 focus:border-site focus:outline-none md:px-3 md:py-2 md:rounded-md md:focus:rounded-lg lg:focus:rounded-xl ease-in duration-100" required />
+                        <div className="flex flex-col justify-star xsz:gap-3 md:gap-y-2">
+                            <label htmlFor="userName" className="font-inter font-normal text-secondary/80 xsz:text-sm lg:text-base"> Name </label>
+                            <input type="text" placeholder="Your Name" id="userName" className="font-poppins xsz:text-[12px] sm:text-sm border-2 md:opacity-80 xsz:border-secondary/30  md:border-secondary/50 focus:border-2 focus:border-site focus:outline-none xsz:rounded-sm xsz:focus:rounded-md xsz:px-2 xsz:py-1 sm:px-3 md:px-3 sm:py-2 md:rounded-md md:focus:rounded-lg lg:focus:rounded-xl ease-in duration-100" required />
                         </div>
 
                         {/* user's Mail */}
-                        <div className="flex flex-col justify-start md:gap-y-2">
-                            <label htmlFor="userMail" className="font-inter font-normal text-secondary/80 md:text-sm lg:text-base"> Email </label>
-                            <input type="email" placeholder="your.name@gmail.com" id="userMail" className = "font-poppins md:text-sm border-2 md:opacity-80  border-secondary/50 focus:border-2 focus:border-site focus:outline-none md:px-3 md:py-2 md:rounded-md md:focus:rounded-lg lg:focus:rounded-xl ease-in duration-100" required />
+                        <div className="flex flex-col justify-start xsz:gap-3 md:gap-y-2">
+                            <label htmlFor="userMail" className="font-inter font-normal text-secondary/80 xsz:text-sm lg:text-base"> Email </label>
+                            <input type="email" placeholder="your.name@gmail.com" id="userMail" className="font-poppins xsz:text-[12px] sm:text-sm border-2 md:opacity-80 xsz:border-secondary/30  md:border-secondary/50 focus:border-2 focus:border-site focus:outline-none xsz:rounded-sm xsz:focus:rounded-md xsz:px-2 xsz:py-1 sm:px-3 md:px-3 sm:py-2 md:rounded-md md:focus:rounded-lg lg:focus:rounded-xl ease-in duration-100" required />
                         </div>
 
                         {/* user's Subject or Purpose */}
-                        <div className="flex flex-col justify-start md:gap-y-2">
-                            <label htmlFor="userSubject" className="font-inter font-normal text-secondary/80 md:text-sm lg:text-base"> Subject </label>
-                            <input type="text" placeholder="Subject/Purpose" id="userSubject" className = "font-poppins md:text-sm border-2 md:opacity-80  border-secondary/50 focus:border-2 focus:border-site focus:outline-none md:px-3 md:py-2 md:rounded-md md:focus:rounded-lg lg:focus:rounded-xl ease-in duration-100" required />
+                        <div className="flex flex-col justify-start xsz:gap-3 md:gap-y-2">
+                            <label htmlFor="userSubject" className="font-inter font-normal text-secondary/80 xsz:text-sm lg:text-base"> Subject </label>
+                            <input type="text" placeholder="Subject/Purpose" id="userSubject" className="font-poppins xsz:text-[12px] sm:text-sm border-2 md:opacity-80 xsz:border-secondary/30  md:border-secondary/50 focus:border-2 focus:border-site focus:outline-none xsz:rounded-sm xsz:focus:rounded-md md:px-3 xsz:px-2 xsz:py-1 sm:px-3 sm:py-2 md:rounded-md md:focus:rounded-lg lg:focus:rounded-xl ease-in duration-100" required />
                         </div>
 
-                        <div className="flex flex-col justify-start md:gap-y-2">
-                            <label htmlFor="userMessage" className="font-inter font-normal text-secondary/80 md:text-sm lg:text-base"> Message </label>
-                            <textarea type="text" rows = "4" placeholder="Your Message here..." id="userMessage" className = "resize-none font-poppins md:text-sm border-2 md:opacity-80  border-secondary/50 focus:border-2 focus:border-site focus:outline-none md:px-3 md:py-2 md:rounded-md md:focus:rounded-lg lg:focus:rounded-xl ease-in duration-100" > </textarea>
+                        <div className="flex flex-col justify-start xsz:gap-3 md:gap-y-2">
+                            <label htmlFor="userMessage" className="font-inter font-normal text-secondary/80 xsz:text-sm lg:text-base"> Message </label>
+                            <textarea type="text" rows="4" placeholder="Your Message here..." id="userMessage" className="resize-none font-poppins xsz:text-[12px] sm:text-sm border-2 md:opacity-80 xsz:border-secondary/30  md:border-secondary/50 focus:border-2 focus:border-site focus:outline-none xsz:rounded-sm xsz:focus:rounded-md xsz:px-2 xsz:py-1 sm:px-3 md:px-3 sm:py-2 md:rounded-md md:focus:rounded-lg lg:focus:rounded-xl ease-in duration-100" > </textarea>
                         </div>
 
-                        <button type="button" className = "font-inter bg-site/70 md:py-2 md:rounded-md lg:rounded-lg hover:rounded-xl hover:bg-site/90 cursor-pointer md:shadow-md hover:shadow-lg ease-in duration-150 text-white md:text-sm md:mt-2"> Send Message </button>
+                        <button type="button" className="font-inter bg-site/70 xsz:py-1 xsz:rounded-sm sm:rounded-md xsz:shadow-lg active:scale-95 md:py-2 md:rounded-md lg:rounded-lg hover:rounded-xl hover:bg-site/90 cursor-pointer md:shadow-md hover:shadow-lg ease-in duration-150 text-white xsz:text-sm md:mt-2"> Send Message </button>
 
                     </form>
 
